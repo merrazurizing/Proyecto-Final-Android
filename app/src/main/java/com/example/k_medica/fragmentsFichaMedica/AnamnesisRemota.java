a50196afbf8785dceeb262388b369baf5130e8d1
fsantander <fsantander@ing.ucsc.cl>
2021-08-05 13:42:36 -0400

    Guardar AmnesisRemota en realm

--- app/src/main/java/com/example/k_medica/fragmentsFichaMedica/AnamnesisRemota.java
+++ app/src/main/java/com/example/k_medica/fragmentsFichaMedica/AnamnesisRemota.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.k_medica.R;
 import com.example.k_medica.models.FichaAnamnesisRemota;
@@ -26,7 +28,8 @@ public class AnamnesisRemota extends Fragment {
     private FichaAnamnesisRemota remota;
     private String idFicha;
     private Realm mRealm;
-
+    private Button btn;
+    private boolean estado;
 
 
     public AnamnesisRemota(String idFicha) {
@@ -43,6 +46,9 @@ public class AnamnesisRemota extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_anamnesis_remota, container, false);
+
+        btn = v.findViewById(R.id.remota_btn);
+        comportamientoInputs(false);
 
         setUpRealmConfig();
         mRealm = Realm.getDefaultInstance();
@@ -70,6 +76,12 @@ public class AnamnesisRemota extends Fragment {
         drogas.setChecked(remota.isDrogas());
         tabaco.setChecked(remota.isTabaco());
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               comportamientoInputs(estado);
+            }
+        });
 
 
         return inflater.inflate(R.layout.fragment_anamnesis_remota, container, false);
@@ -88,4 +100,43 @@ public class AnamnesisRemota extends Fragment {
         Realm.setDefaultConfiguration(config);
 
     }
+
+    private void comportamientoInputs(boolean bol){
+        this.estado = !bol;
+        //bol = false todo se desactiva
+        //bol = true todo se activa
+
+        morbidos.setEnabled(bol);
+        quirurgicos.setEnabled(bol);
+        hospitalizaciones.setEnabled(bol);
+        alergias.setEnabled(bol);
+        alimentacion.setEnabled(bol);
+        drogas.setEnabled(bol);
+        alcohol.setEnabled(bol);
+        tabaco.setEnabled(bol);
+
+        if(!bol){
+            //si bol es falso, pues se desactivó el botón y habría que guardar las cosas en realm
+            //en teoría esto puede quedar en blanco
+
+            remota.setAntecedentes_morbidos(morbidos.getText().toString());
+            remota.setAntecedentes_quirurgicos(quirurgicos.getText().toString());
+            remota.setHospitalizaciones(hospitalizaciones.getText().toString());
+            remota.setAlergias(alergias.getText().toString());
+            remota.setAlimentacion(alimentacion.getText().toString());
+
+            remota.setDrogas(drogas.isChecked());
+            remota.setAlcohol(alcohol.isChecked());
+            remota.setTabaco(tabaco.isChecked());
+
+            remota.setSendBd(false);
+
+            mRealm.beginTransaction();
+            mRealm.insertOrUpdate(remota);
+            mRealm.commitTransaction();
+
+        }
+    }
+
+
 }
